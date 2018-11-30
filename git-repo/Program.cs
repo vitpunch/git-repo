bde764faf9b2277dd3d27e0ae8f042fc4f47a17f
vitpunch <vitpunch@yandex.ru>
2018-11-30 18:26:05 +0800

    урок 9

--- git-repo/Program.cs
+++ git-repo/Program.cs
@@ -10,8 +10,14 @@ namespace git_repo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("What");
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ git-repo/VerticalLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_repo
+{
+    class VerticalLine : Figure
+    {        
+        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }        
+    }
+}
